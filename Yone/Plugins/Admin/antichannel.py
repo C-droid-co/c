c57d8fab5feee311f2f2546195823841e6f00721
Fintechbot <103402546+Fintechbot@users.noreply.github.com>
2022-04-24 07:24:59 +0100

    Update antichannel.py

--- Yone/Plugins/Admin/antichannel.py
+++ Yone/Plugins/Admin/antichannel.py
@@ -83,3 +83,10 @@ __handlers__ = [
     (ANTICHANNEL_HANDLER, SET_CH_GROUP),
     (ELIMINATE_CHANNEL_HANDLER, ELEMINATE_CH_GROUP),
 ]
+__help__ = """
+Restrict users from sending as anonymous channels
+ • `/antichannel <on/off/yes/no>`*:* enables antichannel in the current chat
+If enabled, the message from the channel which the user sends will be banned.
+"""
+
+__mod_name__ = "Antichannel"
